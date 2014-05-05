61b7570e1b34e8f9119836cc7372d98ebd76288d
funkyHui <540886643@qq.com>
2014-05-05 09:13:50 +0800

    task

---
+++ api/models/Task.js
@@ -0,0 +1,29 @@
+/**
+ * Task
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+  adapter: 'mysql',
+  schema: true,
+  tableName : 'tasks',
+  attributes: {
+    title: {
+      type: 'string',
+      required: true
+    },
+    descp: {
+      type: 'string',
+      maxLength: 500,
+      required: true
+    },
+    publisherId: {
+      type: 'string',
+      required: true
+    },
+    accepterId: 'string'
+  }
+};
