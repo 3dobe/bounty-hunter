12354496c61d29c8bd20718075065d35d08be0f7
funkyHui <540886643@qq.com>
2014-05-14 14:38:01 +0800

    测试

--- api/controllers/StateController.js
+++ api/controllers/StateController.js
@@ -18,6 +18,7 @@
 var Client = require('../../assets/js/client');
 var parseProfile = require('../../assets/js/parseProfile');
 var async = require('async');
+var crypto = require('crypto');
 module.exports = {
   //登陆操作
   login: function (req, res) {
@@ -33,26 +34,26 @@ module.exports = {
       Admin.findOne({
         username: req.body['username']
       }).done(function (err, admin) {
-        console.log(admin);
-        if (err) {
-          console.log(err);
-          return res.redirect('/login');
-        }
-        if (!admin) {
-          //登陆信息错误处理
-          return res.redirect('/login');
-        } else {
-          //密码错误处理
-          if (admin.password === req.body['password']) {
-            //用户状态保存
-            req.session['admin'] = admin;
             console.log(admin);
-            return res.redirect('/');
-          } else {
-            return res.redirect('/login');
-          }
-        }
-      });
+            if (err) {
+              console.log(err);
+              return res.redirect('/login');
+            }
+            if (!admin) {
+              //登陆信息错误处理
+              return res.redirect('/login');
+            } else {
+              //密码错误处理
+              if (admin.password === req.body['password']) {
+                //用户状态保存
+                req.session['admin'] = admin;
+                console.log(admin);
+                return res.redirect('/');
+              } else {
+                return res.redirect('/login');
+              }
+            }
+          });
     }
 
     if (option === 'user') {
@@ -63,34 +64,34 @@ module.exports = {
       };
 
       //如果已经匹配成功,直接转页
-      if(req.session['user']) {
+      if (req.session['user']) {
         //直接转页
         return res.redirect('/');
       }
 
       //数据库中没有相应用户
-      if(req.body['noUser']) {
+      if (req.body['noUser']) {
         //4.2 数据中没有相应记录,则发送请求道学生服务子系统进行抓取,;
         var client = new Client('jwc.wyu.cn', '/student', 'gbk');
         async.waterfall([
-          function(next) {
+          function (next) {
             client.get('/createsession_a.asp', {}, {}, next(null));
-          }, function(next) {
+          }, function (next) {
             client.get('/createsession_b.asp', {}, {}, next(null));
-          }, function(next) {
+          }, function (next) {
             client.get('/rndnum.asp', {}, {}, next(null));
-          }, function(next) {
+          }, function (next) {
             client.cookie['LogonNumber'] = '';
             client.post('/logon.asp', data, {
               Referer: 'http://jwc.wyu.cn/student/body.htm'
             }, function (err, res, body) {
-              if(err) {
+              if (err) {
                 next(new Error('登陆出错'));
               } else {
                 next(null, res, body);
               }
             });
-          }, function(res, body, next) {
+          }, function (res, body, next) {
             var success = /welcome/.test(body);
             if (success) {
               client.get('/f1.asp', {}, {}, function (err, res, body) {
@@ -105,11 +106,11 @@ module.exports = {
                     class: profile.class,
                     dormNo: profile.dormitory,
                     condition: false
-                  }).done(function(err, user) {
+                  }).done(function (err, user) {
                         if (err) {
                           console.log(err);
                           next(new Error('新增数据出错'));
-                        }else {
+                        } else {
                           console.log("User created:", user);
                           next(null, user);
                         }
@@ -124,8 +125,8 @@ module.exports = {
               next(new Error('登陆出错'));
             }
           }
-        ], function(err, user) {
-          if(err) {
+        ], function (err, user) {
+          if (err) {
             console.log(err);
             return res.redirect('/');
           } else {
@@ -136,35 +137,35 @@ module.exports = {
       }
 
       //密码不匹配
-      if(!req.body['noUser'] && req.body['wrongPw']) {
+      if (!req.body['noUser'] && req.body['wrongPw']) {
         //4.1.1 如密码不匹配,则发送请求到学生服务子系统进行抓取,
         var client = new Client('jwc.wyu.cn', '/student', 'gbk');
         async.waterfall([
-          function(next) {
+          function (next) {
             client.get('/createsession_a.asp', {}, {}, next(null));
-          }, function(next) {
+          }, function (next) {
             client.get('/createsession_b.asp', {}, {}, next(null));
-          }, function(next) {
+          }, function (next) {
             client.get('/rndnum.asp', {}, {}, next(null));
-          }, function(next) {
+          }, function (next) {
             client.cookie['LogonNumber'] = '';
             client.post('/logon.asp', data, {
               Referer: 'http://jwc.wyu.cn/student/body.htm'
             }, function (err, res, body) {
-              if(err) {
+              if (err) {
                 next(new Error('登陆出错'));
               } else {
                 next(null, res, body);
               }
             });
-          }, function(res, body, next) {
+          }, function (res, body, next) {
             console.log(body);
             var success = /welcome/.test(body);
             if (success) {
               //4.1.1.1 抓取成功,更新数据到数据库,保存用户信息到session,转页
               user.password = data.UserPwd;
-              user.save(function(err) {
-                if(err) {
+              user.save(function (err) {
+                if (err) {
                   console.log(err);
                   next(new Error("更新出错"));
                 } else {
@@ -176,8 +177,8 @@ module.exports = {
               next(new Error('密码错误'));
             }
           }
-        ], function(err, user) {
-          if(err) {
+        ], function (err, user) {
+          if (err) {
             console.log(err);
             return res.redirect('/');
           } else {
@@ -191,9 +192,9 @@ module.exports = {
 
   //登出操作
   logout: function (req, res) {
-    if(req.session['admin']) {
+    if (req.session['admin']) {
       delete req.session['admin'];
-    } else if(req.session['user']) {
+    } else if (req.session['user']) {
       delete req.session['user'];
     }
     return res.redirect('/');
