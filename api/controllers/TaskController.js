e3cb4b74eb7f4a5c4481739a8afe3cd892f19a60
funkyHui <540886643@qq.com>
2014-06-04 11:44:47 +0800

    TaskController.js

--- api/controllers/TaskController.js
+++ api/controllers/TaskController.js
@@ -18,17 +18,79 @@
 module.exports = {
   myTasksPublish : function(req, res) {
     //用户发布的任务列表
+    var id = req.session['user'].id;
+    Task.find({
+      publisherId : id
+    }).done(function (err, tasks) {
+      if(err) {
+        console.log(err);
+      } else if (tasks==null){
+        return res.send({ msg : 'no task' });
+      } else {
+        return res.send({ tasks : tasks });
+      }
+    });
   },
 
   myTasksAccept : function(req, res) {
     //用户接受的任务列表
+    var id = req.session['user'].id;
+    Task.find({
+      accepterId : id
+    }).done(function (err, tasks) {
+          if(err) {
+            console.log(err);
+          } else if (tasks==null){
+            return res.send({ msg : 'no task' });
+          } else {
+            return res.send({ tasks : tasks });
+          }
+        });
   },
 
   acceptTask : function(req, res) {
     //用户接受任务
+    var tid = req.query['id'];
+    var id = req.session['user'].id;
+    Task.findOne({
+      id : tid,
+      accepterId : null
+    }).done(function (err, task) {
+      if(err) {
+        console.log(err);
+      }
+      if(!task) {
+        return res.send({ msg : 'no task'})
+      }
+      task.accepterId = id;
+      task.save(function(err) {
+        if(err) {
+          return res.send({ msg : 'accept fail' })
+        }
+      });
+    });
   },
 
   fulfilTask : function(req, res) {
     //完成任务
+    var tid = req.query['id'];
+    var id = req.session['user'].id;
+    Task.findOne({
+      id : tid,
+      isFinish : false
+    }).done(function (err, task) {
+          if(err) {
+            console.log(err);
+          }
+          if(!task) {
+            return res.send({ msg : 'no task'})
+          }
+          task.isFinish = true;
+          task.save(function(err) {
+            if(err) {
+              return res.send({ msg : 'finish fail' })
+            }
+          });
+        });
   }
 };
